cd7477482eaa1c3dabfcb4b3d2d5a0193702f4ef
nathanielltaylor <ntaylor390@gmail.com>
2015-11-17 20:33:09 -0500

    merge sort

--- sort.rb
+++ sort.rb
@@ -23,3 +23,30 @@ def insertion_sort(array, element_count)
   end
   return array
 end
+
+def merge_sort(array, low, high)
+  return array if low >= high
+  middle = (low + high) / 2
+  merge_sort(array, low, middle)
+  merge_sort(array, middle + 1, high)
+  merge(array, low, middle, high)
+end
+
+def merge(array, low, middle, high)
+  lower_half = array[low..middle]
+  upper_half = array[(middle + 1)..high]
+  upper_half.push("ZZZZZ")
+  lower_half.push("ZZZZZ")
+  i = 0
+  j = 0
+  for k in (low..high)
+    if lower_half[i] <= upper_half[j]
+      array[k] = lower_half[i]
+      i += 1
+    else
+      array[k] = upper_half[j]
+      j += 1
+    end
+  end
+  return array
+end
--- timer.rb
+++ timer.rb
@@ -1,10 +1,11 @@
 require 'benchmark'
 require_relative 'sort'
 
-names = IO.read('names.txt').split(',')
-
 Benchmark.bm(30) do |x|
+  names = IO.read('names.txt').split(',')
   x.report("Selection Sort:") { selection_sort(names, names.length) }
   names = IO.read('names.txt').split(',')
   x.report("Insertion Sort:") { insertion_sort(names, names.length) }
+  names = IO.read('names.txt').split(',')
+  x.report("Merge Sort:") { merge_sort(names, 0, (names.length - 1)) }
 end
